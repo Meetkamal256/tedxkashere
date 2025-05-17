38b0eacf4ca0f7f6c1687eed1db0764cd70f9ca8
meetkamal256 <meetkamal256@gmail.com>
2025-05-17 13:47:57 +0100

    Add Intro component JSX structure

---
+++ app/components/intro/Intro.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import styles from "./intro.module.css";
+import Link from "next/link";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const Intro = () => {
+  return (
+    <section className="container">
+      <div className={styles.intro}>
+        <div className={styles.content}>
+          <h2 className={styles.heading}>What is TEDxFUKashere?</h2>
+          <p className={styles.description}>
+            TEDxFUKashere is an independently organized TED event that brings
+            together visionaries, thinkers, and changemakers from our university
+            and community to share their ideas worth spreading. Our mission is
+            to spark deep conversation and connection through powerful talks and
+            stories.
+          </p>
+          <Link href="/about" className={styles.learnMore}>
+            Learn more
+            <FaArrowRightLong className="arrowRightIcon" />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import HeroSection from "./components/sections/hero/HeroSection";
+import Intro from "./components/intro/Intro";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Intro />
     </>
   );
 }
